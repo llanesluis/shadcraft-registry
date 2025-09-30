0410c792319185d21e2ff0d452cf3774e63cbb52
Luis Llanes <luisllaboj@gmail.com>
2025-09-29 17:21:24 -0700

    chore: Basic devtools config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,6 +12,20 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
+      rules:{
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          args: "all",
+          argsIgnorePattern: "^_",
+          caughtErrors: "all",
+          caughtErrorsIgnorePattern: "^_",
+          destructuredArrayIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          ignoreRestSiblings: true,
+        },
+      ],
+    },
     ignores: [
       "node_modules/**",
       ".next/**",
