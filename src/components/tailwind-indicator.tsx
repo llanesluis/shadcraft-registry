e223759de70cc6570315cb3ef73e16d8f284b8a8
Luis Llanes <luisllaboj@gmail.com>
2025-10-01 14:28:55 -0700

    chore: providers and clean boilerplate code

---
+++ src/components/tailwind-indicator.tsx
@@ -0,0 +1,21 @@
+const SHOW = true;
+
+export function TailwindIndicator() {
+  if (process.env.NODE_ENV === "production" || !SHOW) {
+    return null;
+  }
+
+  return (
+    <div
+      data-tailwind-indicator=""
+      className="bg-muted text-foreground fixed bottom-1 left-1 z-50 flex size-8 items-center justify-center rounded-full p-3 font-mono text-xs"
+    >
+      <div className="block sm:hidden">xs</div>
+      <div className="hidden sm:block md:hidden">sm</div>
+      <div className="hidden md:block lg:hidden">md</div>
+      <div className="hidden lg:block xl:hidden">lg</div>
+      <div className="hidden xl:block 2xl:hidden">xl</div>
+      <div className="hidden 2xl:block">2xl</div>
+    </div>
+  );
+}
