d0d7836cc578f33fc615798dd6c3724569943896
Luis Llanes <luisllaboj@gmail.com>
2025-10-06 19:25:20 -0700

    chore: disable nextjs img lint rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,6 +13,7 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
       rules:{
+      "@next/next/no-img-element": "off",
       "@typescript-eslint/no-unused-vars": [
         "error",
         {
