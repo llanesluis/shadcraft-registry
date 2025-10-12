ea04f9d91f4e03c829a07e022ecc7e493244d1ce
Luis Llanes <luisllaboj@gmail.com>
2025-10-11 18:20:44 -0700

    feat: add tweakcn live preview script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,6 +29,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <script crossOrigin="anonymous" src="https://tweakcn.com/live-preview.js" />
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable} font-sans antialiased`}>
         <ThemeProvider>
           {children}
