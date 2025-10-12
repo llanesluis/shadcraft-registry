bf2551a8d9bbf477e31e5c82b1da58fc200194df
Luis Llanes <luisllaboj@gmail.com>
2025-10-11 18:22:35 -0700

    fix: async script?

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <script crossOrigin="anonymous" src="https://tweakcn.com/live-preview.js" />
+        <script async crossOrigin="anonymous" src="https://tweakcn.com/live-preview.js" />
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} font-sans antialiased`}>
         <ThemeProvider>
