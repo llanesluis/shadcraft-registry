92bfa653ebd34488ea452e2c6bd4df3357a46b6d
Luis Llanes <luisllaboj@gmail.com>
2025-10-11 19:01:04 -0700

    chore: change tweakcn script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <script async crossOrigin="anonymous" src="https://tweakcn.com/live-preview.js" />
+        <script async crossOrigin="anonymous" src="https://tweakcn.com/live-preview.min.js" />
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} font-sans antialiased`}>
         <ThemeProvider>
