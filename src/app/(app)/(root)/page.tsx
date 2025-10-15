460611feb9da422676de09a5ccaa6f9e569cc02f
Luis Llanes <luisllaboj@gmail.com>
2025-10-15 16:56:51 -0700

    chore: home page show blocks by category

--- src/app/(app)/(root)/page.tsx
+++ src/app/(app)/(root)/page.tsx
@@ -1,71 +1,83 @@
+import { ArrowUpRight } from "lucide-react";
 import Link from "next/link";
 
 import { PageHeader, PageHeaderDescription, PageHeaderHeading } from "@/components/page-header";
 import { getBlockItems, getComponentItems, getUiItems } from "@/lib/registry";
-import { ArrowUpRight } from "lucide-react";
+import { groupBlocksByCategories } from "@/utils/registry";
 
 export default async function HomePage() {
   const registryUiItems = getUiItems();
+
   const registryComponentItems = getComponentItems();
-  const registryBlockItems = getBlockItems();
+
+  const blockItems = getBlockItems().filter((item) => item.type === "registry:block");
+  const blockItemsByCategories = groupBlocksByCategories(blockItems);
 
   return (
-    <main className="space-y-10">
+    <>
       <PageHeader>
         <span className="code-inline w-fit font-mono font-medium">registry</span>
         <PageHeaderHeading>Shadcraft Registry</PageHeaderHeading>
         <PageHeaderDescription>
-          A collection of UI elements, components, blocks, and templates for Shadcraft.
+          A collection of UI elements, components, blocks, and tem plates for Shadcraft.
         </PageHeaderDescription>
       </PageHeader>
 
-      <section className="space-y-4">
-        <h2 className="text-2xl font-semibold">UI</h2>
-        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-          {registryUiItems.map((item) => (
-            <Link
-              href={`/ui/${item.name}`}
-              key={item.name}
-              className="group/link flex items-center gap-2 font-mono"
-            >
-              {item.title}
-              <ArrowUpRight className="size-4 opacity-0 transition-all group-hover/link:opacity-100" />
-            </Link>
-          ))}
-        </div>
-      </section>
+      <div className="space-y-16">
+        <section className="space-y-4">
+          <span className="code-inline w-fit font-mono font-medium">registry:ui</span>
+          <h2 className="text-2xl font-semibold">UI</h2>
+          <div className="grid grid-cols-1 gap-x-8 gap-y-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {registryUiItems.map((item) => (
+              <Link
+                key={item.name}
+                href={`/ui/${item.name}`}
+                className="group/link flex items-center justify-between gap-2 font-mono"
+              >
+                {item.title}
+                <ArrowUpRight className="size-4 opacity-0 transition-all group-hover/link:opacity-100" />
+              </Link>
+            ))}
+          </div>
+        </section>
 
-      <section className="space-y-4">
-        <h2 className="text-2xl font-semibold">Components</h2>
-        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-          {registryComponentItems.map((item) => (
-            <Link
-              href={`/components/${item.name}`}
-              key={item.name}
-              className="group/link flex items-center gap-2 font-mono"
-            >
-              {item.title}
-              <ArrowUpRight className="size-4 opacity-0 transition-all group-hover/link:opacity-100" />
-            </Link>
-          ))}
-        </div>
-      </section>
+        <section className="space-y-4">
+          <span className="code-inline w-fit font-mono font-medium">registry:component</span>
+          <h2 className="text-2xl font-semibold">Components</h2>
+          <div className="grid grid-cols-1 gap-x-8 gap-y-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {registryComponentItems.map((item) => (
+              <Link
+                key={item.name}
+                href={`/components/${item.name}`}
+                className="group/link flex items-center justify-between gap-2 font-mono"
+              >
+                {item.title}
+                <ArrowUpRight className="size-4 opacity-0 transition-all group-hover/link:opacity-100" />
+              </Link>
+            ))}
+          </div>
+        </section>
 
-      <section className="space-y-4">
-        <h2 className="text-2xl font-semibold"> Blocks</h2>
-        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-          {registryBlockItems.map((item) => (
-            <Link
-              href={`/blocks/${item.name}`}
-              key={item.name}
-              className="group/link flex items-center gap-2 font-mono"
-            >
-              {item.title}
-              <ArrowUpRight className="size-4 opacity-0 transition-all group-hover/link:opacity-100" />
-            </Link>
-          ))}
-        </div>
-      </section>
-    </main>
+        <section className="space-y-4">
+          <span className="code-inline w-fit font-mono font-medium">registry:block</span>
+          <h2 className="text-2xl font-semibold"> Blocks</h2>
+          <div className="grid grid-cols-1 gap-x-8 gap-y-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {blockItemsByCategories.map((item) => (
+              <Link
+                key={item.slug}
+                href={`/blocks/${item.slug}`}
+                className="group/link flex items-center justify-between gap-2 font-mono"
+              >
+                {item.title}
+                <span className="flex items-center gap-2">
+                  <span className="text-muted-foreground">{item.amount}</span>
+                  <ArrowUpRight className="size-4 opacity-0 transition-all group-hover/link:opacity-100" />
+                </span>
+              </Link>
+            ))}
+          </div>
+        </section>
+      </div>
+    </>
   );
 }
